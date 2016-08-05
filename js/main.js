a86e8af25136995dbde28c5dfd2fa1931a8a2a45
Julian Selser <julianselser@yahoo.com.ar>
2016-08-04 23:27:29 -0300

    Added background image. Hair positioning on click, it also changes with the power level

--- js/main.js
+++ js/main.js
@@ -22,45 +22,66 @@
  * THE SOFTWARE.
  */
 
-document.getElementById('powerRange').oninput = function(){
-    console.log(this.value);
+
+document.getElementById('sayayinHair').onclick = function (event) {
+    var clickPosition = getCursorPosition(this, event);
+
+    drawHairOnCanvas(clickPosition);
 };
 
-document.getElementById('fakeImageSelector').onclick = function(){
+document.getElementById('powerRange').oninput = drawHairOnCanvas;
+
+document.getElementById('fakeImageSelector').onclick = function () {
     document.getElementById('imageSelector').click();
 };
 
-document.getElementById('imageSelector').onchange = function(eventImageSelected) {
+document.getElementById('imageSelector').onchange = function (eventImageSelected) {
     var file = eventImageSelected.target.files[0];
     var fileReader = new FileReader();
 
-    fileReader.onload = function(eventImageLoaded) {
-        var hairCanvas = document.getElementById('sayayinHair');
-        var powerCanvas = document.getElementById('powerLevel');
+    fileReader.onload = function (eventImageLoaded) {
         var imageToSayaynize = document.getElementById('imageToSayanize');
-        
+
         imageToSayaynize.src = eventImageLoaded.target.result;
-        
-        powerCanvas.height = hairCanvas.height = imageToSayaynize.height;
-        powerCanvas.width = hairCanvas.width = imageToSayaynize.width;
-        
-        var ctx = hairCanvas.getContext('2d');
-        var hairImg = new Image();
 
-        hairImg.onload = function(){
-            var biggerHairImgSide = Math.max(hairImg.height, hairImg.width);
-            var biggerUserImgSide = Math.min(imageToSayaynize.height, imageToSayaynize.width);
-            var scaleRatio = biggerHairImgSide / (biggerUserImgSide/2);
-            var scaleRatio = (scaleRatio > 1)? 1/scaleRatio : scaleRatio;
-            
-//            console.log(imageToSayaynize.height, imageToSayaynize.width);
-//            console.log(scaleRatio, biggerHairImgSide, biggerUserImgSide);
-            
-            ctx.drawImage(hairImg, 0, 0, hairImg.width*scaleRatio, hairImg.height*scaleRatio);
-        };
-
-        hairImg.src = 'img/saiyanHair.png';
+        drawHairOnCanvas();
     };
 
     fileReader.readAsDataURL(file);
 };
+
+function getCursorPosition(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
+}
+
+function drawHairOnCanvas(position) {
+    var imageToSayaynize = document.getElementById('imageToSayanize');
+    var hairCanvas = document.getElementById('sayayinHair');
+    var powerCanvas = document.getElementById('powerLevel');
+    var ctx = hairCanvas.getContext('2d');
+    var hairImg = new Image();
+
+    powerCanvas.height = hairCanvas.height = imageToSayaynize.height;
+    powerCanvas.width = hairCanvas.width = imageToSayaynize.width;
+
+    hairImg.onload = function () {
+        var biggerHairImgSide = Math.max(hairImg.height, hairImg.width);
+        var biggerUserImgSide = Math.min(hairCanvas.height, hairCanvas.width);
+        var scaleRatio = biggerHairImgSide / (biggerUserImgSide / 2);
+        var scaleRatio = (scaleRatio > 1) ? 1 / scaleRatio : scaleRatio;
+        
+        ctx.drawImage(hairImg,
+                position.x - (hairImg.width * scaleRatio) / 2,
+                position.y - (hairImg.height * scaleRatio) / 2,
+                hairImg.width * scaleRatio, hairImg.height * scaleRatio);
+    };
+
+    hairImg.src = document.getElementById('powerRange').value > 80 
+        ? 'img/superSaiyanHair.png' 
+        : 'img/normalSaiyanHair.png';
+}
